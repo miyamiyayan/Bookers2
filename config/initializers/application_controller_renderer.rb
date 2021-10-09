13b5f2a27efb1c0ae1c27aa20f488a2a5d74a788
miyamiyayan <dmkm.25.active@gmail.com>
2021-10-09 02:17:51 +0000

    [update]仕様完成、次は権限関係

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2e1ddbb349e296b21a034a1b42817c08268eabde8998f24b8c2b0cab3de477415d028f65e47a35ca1d75195e0c54aa4a2495c2183fafa25b9aafd96b09d00017'

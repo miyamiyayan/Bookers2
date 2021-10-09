94ed5de482a1bb5a9ff7b2c9dcb18639028ea347
miyamiyayan <dmkm.25.active@gmail.com>
2021-10-09 03:26:43 +0000

    [update]バリデーション完成、Nextビューに反映

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :book, dependent: :destroy
   attachment :profile_image
 
+  validates :name, presence: true, length: {minimum: 2,maximum: 50 }, uniqueness: true
+  validates :introduction, length: {maximum: 50 }
+
+
 end
